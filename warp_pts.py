81c995aaa1fa0fc7b1db19191c6604516e0c803f
Jayadev1402 <89912646+Jayadev1402@users.noreply.github.com>
2023-01-26 22:39:21 -0500

    Update warp_pts.py

--- warp_pts.py
+++ warp_pts.py
@@ -4,9 +4,6 @@ from est_homography import est_homography
 
 def warp_pts(X, X_prime, interior_pts):
     """
-    First compute homography from video_pts to logo_pts using X and X_prime,
-    and then use this homography to warp all points inside the soccer goal
-
     Input:
         X: 4x2 matrix of (x,y) coordinates of goal corners in video frame
         X_prime: 4x2 matrix of (x,y) coordinates of logo corners in penn logo
@@ -14,15 +11,14 @@ def warp_pts(X, X_prime, interior_pts):
     Returns:
         warped_pts: Nx2 matrix containing new coordinates for interior_pts.
         These coordinate describe where a point inside the goal will be warped
-        to inside the penn logo. For this assignment, you can keep these new
-        coordinates as float numbers.
+        to inside the penn logo. 
 
     """
 
-    # You should Complete est_homography first!
+
     H = est_homography(X, X_prime)
 
-    ##### STUDENT CODE START #####
+
     warp=[]
 
     for i in range(0,interior_pts.shape[0]):
@@ -32,7 +28,4 @@ def warp_pts(X, X_prime, interior_pts):
         warp.append([xp,yp])
         
     warped_pts = np.array(warp)
-    # print(warped_pts.shape)
-    ##### STUDENT CODE END #####
-    # print(np.shape(warped_pts))
     return warped_pts
